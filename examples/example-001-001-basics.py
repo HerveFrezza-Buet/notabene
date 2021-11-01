552b610c9eb4f79474712dfcac9e8e664c71b88c
Hervé Frezza-Buet <Herve.Frezza-Buet@centralesupelec.fr>
2021-11-01 12:42:24 +0100

    updt

--- examples/example-001-001-basics.py
+++ examples/example-001-001-basics.py
@@ -11,7 +11,14 @@ sigma      = nb.to('\\sigma')
 
 # Then, basic operators can be used to combine formulas... let us
 # illustrate some of them and put some results in the file basics.tex.
-# We name latex commands \exA, \exB... thanks chr(65+i).
+
+# This is a trick to get nen command names
+name_rank = 0
+def new_name():
+    global name_rank
+    name = chr(65 + name_rank) # name = 'A', 'B', 'C', ...
+    name_rank += 1
+    return name
 
 with nb.files.defs('basics.tex') as defs:
     nb.set_displaystyle(True) # We set the displaystyle
@@ -19,83 +26,66 @@ with nb.files.defs('basics.tex') as defs:
     idx = 0
 
     nb.set_product_mode(None) #default
-    defs[chr(65 + idx)] = x*y
-    idx += 1
+    defs[new_name()] = x*y
     
     nb.set_product_mode('.') 
-    defs[chr(65 + idx)] = x*y
-    idx += 1
+    defs[new_name()] = x*y
     
     nb.set_product_mode('x') 
-    defs[chr(65 + idx)] = x*y
-    idx += 1
+    defs[new_name()] = x*y
     
     nb.set_product_mode(None)
     
     e = 1 + sigma**2 / (-(x+y)**(x-y) + x*y)
-    defs[chr(65 + idx)] = e
-    idx += 1
+    defs[new_name()] = e
 
     euler = nb.math.e**(nb.math.i * nb.math.pi) + 1 == 0
-    defs[chr(65 + idx)] = euler
-    idx += 1
+    defs[new_name()] = euler
 
-    defs[chr(65 + idx)] = nb.define(nb.math.i, nb.math.sqrt(-1))
-    idx += 1
+    defs[new_name()] = nb.define(nb.math.i, nb.math.sqrt(-1))
 
     # nb.seq builds a formula that is a sequence of others.
-    defs[chr(65 + idx)] = nb.seq(x*y, x^y, x**y, x@y, x@(y,sigma), x == y)
-    idx += 1    
+    defs[new_name()] = nb.seq(x*y, x^y, x**y, x@y, x@(y,sigma), x == y)    
 
     # Quantifiers are available
-    defs[chr(65 + idx)] = nb.math.forall(x, x**2 >= 0)
-    idx += 1
-    defs[chr(65 + idx)] = nb.math.exists(x, x**2 == 0)
-    idx += 1
+    defs[new_name()] = nb.math.forall(x, x**2 >= 0)
+    defs[new_name()] = nb.math.exists(x, x**2 == 0)
 
     # nb.cat concatenates formulas, nb.kat does the same with spacing.
-    defs[chr(65 + idx)] = nb.cat(x**2, y**3, x**n)
-    idx += 1
-    defs[chr(65 + idx)] = nb.kat(x**2, y**3, x**n)
-    idx += 1
+    defs[new_name()] = nb.cat(x**2, y**3, x**n)
+    defs[new_name()] = nb.kat(x**2, y**3, x**n)
 
     # Sum and products
     sum1 = nb.math.sum(i==0, i <= n, 1/nb.math.fact(i) * x**i)
     sum2 = nb.math.sum(nb.sets.isin(i, nb.sets.N), 1 / i**2)
     sum3 = nb.math.prod(nb.sets.isin(i, nb.sets.N), 1 / i**2)
-    defs[chr(65 + idx)] = nb.seq(sum1, sum2, sum3)
-    idx += 1
+    defs[new_name()] = nb.seq(sum1, sum2, sum3)
 
     # You can define functions.
     fname = nb.to('f')@(nb.seq(sigma, i), nb.math.pi)
     f = nb.fun(fname)
-    defs[chr(65 + idx)] = f(sum1+sum2)
-    idx += 1
+    defs[new_name()] = f(sum1+sum2)
 
     # Groups and tuples are made from lists... this may happen implicitely
     v = nb.to([n+1/n]) # explicit grouping formula from list.
     k = [n+1/n] ** n   # implicit grouping formula from list/
-    defs[chr(65 + idx)] = nb.seq(v, k)
-    idx += 1
+    defs[new_name()] = nb.seq(v, k)
 
     xy = [x, y, i, 3] # a list, not a formula.
     xy_t = xy**nb.text('hello world') # a list combined with a formula makes a formula.
-    defs[chr(65 + idx)] = xy_t
-    idx += 1
+    defs[new_name()] = xy_t
 
     # Nice attributes are available
     e = nb.to([x+y])
-    defs[chr(65 + idx)] = nb.seq(e.bar, e.inv, e.T, e.star, e.plus, e.minus)
-    idx += 1
+    defs[new_name()] = nb.seq(e.bar, e.inv, e.T, e.star, e.plus, e.minus)
     
     # Delimiters can be added around formulas (the previous is for parentheses).
     x_2 = x ** 2
-    defs[chr(65 + idx)] = nb.seq(nb.math.abs(x_2),
+    defs[new_name()] = nb.seq(nb.math.abs(x_2),
                                  nb.math.norm(x_2),
                                  nb.math.matrix(x_2),
                                  nb.math.left_system(x_2),
                                  nb.math.right_system(x_2))
-    idx += 1
 
     # Layout arrange formulas as tabular. First is 'r', 'l', 'c' for
     # alignement, then the lines come.
@@ -104,16 +94,14 @@ with nb.files.defs('basics.tex') as defs:
                        [x, x**2],
                        [nb.math.pi],
                        [1, 2, 3])
-    defs[chr(65 + idx)] = layout
-    idx += 1
+    defs[new_name()] = layout
 
     # This can be mixed nicely for a system of equations.
     eqs = nb.math.left_system(nb.layout('l',
                                         [x+1, nb.text('if'), x > 0],
                                         [x-1, nb.text('if'), x < 0],
                                         [38, nb.text('otherwise')]))
-    defs[chr(65 + idx)] = f(x) == eqs
-    idx += 1
+    defs[new_name()] = f(x) == eqs
 
     defs.cheatsheet() # This generates a latex cheetsheet that you can compile and display.
 
